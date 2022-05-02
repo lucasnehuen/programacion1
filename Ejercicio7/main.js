2671ec8ca84cdadeced87ef18e438c739b10d2fc
lucasnehuen <quininirlucas@gmail.com>
2022-05-02 17:08:13 -0300

    arreglos

---
+++ Ejercicio7/main.js
@@ -0,0 +1,12 @@
+let arreglos = ["Pyhon","JavaScript","Csharp","C++","Java","PHP","Ruby"]
+
+console.log(`
+longitud= ${arreglos.length}
+`)
+
+let ultimo_elemento = arreglos [arreglos.length-1]
+console.log(`
+ultimo_elemento= ${ultimo_elemento} 
+`)
+
+let indeice = arreglos.indexOf("C++")
