611bd7291584f3df8f61aa2c581272bb9ccb6a22
lucasnehuen <quininirlucas@gmail.com>
2022-05-02 14:18:10 -0300

    tp evaluativo

--- TPEVALUATIVO/EJ1/main.js
+++ TPEVALUATIVO/EJ1/main.js
@@ -1,18 +1,25 @@
-function mostrar()
-{
+    function calcular()
+    {
 
-    const auto = document.getElementById("inp_auto").value
+        const auto = document.getElementById("inp_auto").value
 
-    if(auto=='Ford Ka'){
-        const resultado= 1750000*15/100
-    }else{
-        if(auto == 'Ford Fiesta'){
-            const resultado= 1950000*5/100
+        if(auto=='Ford Ka'){
+            const resultado= 1750000*15/100
+
+            document.getElementById("resultado").textContent=resultado
+
         }else{
-            const resultado= 2560000*10/100
+            if(auto == 'Ford Fiesta'){
+                const resultado= 1950000*5/100
+
+                document.getElementById("resultado").textContent=resultado
+
+            }else{
+                if(auto=='Ford Focus'){
+                const resultado= 2560000*10/100
+                document.getElementById("resultado").textContent=resultado
+            }
         }
+
     }
-
 }
-
-<button onclick="functionToExecute()">Mostrar</button>
