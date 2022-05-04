26d96d8533ae89ecad8a7bf0583678647a30ca39
lucasnehuen <quininirlucas@gmail.com>
2022-05-04 17:03:09 -0300

    Actividad_practica_arreglos2

---
+++ Actividad_practica_arreglos2/main.js
@@ -0,0 +1,24 @@
+let numeros=[5,2,88,14,22,7,6,42,47,34]
+
+function enviar(numeros){
+
+    let crear_copia = numeros.slice();
+
+}
+
+
+let suma=0
+
+numeros.forEach((element, index) => {
+    
+    console.log(`
+    suma +=element
+    `)
+});
+
+console.log(
+    suma
+)
+console.log(suma/numeros.length)
+
+function promedio_rango= 
